d3c0742bbc11c11ad2be35588fe3976ef318102b
Crowdbotics <team@crowdbotics.com>
2021-10-12 01:58:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "green_poetry_31339",
+  url: "https://green_poetry_31339.botics.co",
+  api: "https://green_poetry_31339.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
